2c6b4af2a3796df63d05d484e0a80d7474d77da8
molt0bene <alisar3@yandex.ru>
2022-09-02 22:56:05 +0300

    extra migration removed

--- db/migrate/20220902170713_remove_extra_index.rb
+++
@@ -1,6 +0,0 @@
-class RemoveExtraIndex < ActiveRecord::Migration[5.2]
-  # it was created by mistake
-  def change
-    remove_index :users, column: [nil], name: "index_users_on_emails"
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_09_02_170713) do
+ActiveRecord::Schema.define(version: 2022_09_02_165813) do
 
   create_table "questions", force: :cascade do |t|
     t.text "body"
