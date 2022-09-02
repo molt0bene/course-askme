483a263efd103cc22042640fbf2399bc06219b1f
molt0bene <alisar3@yandex.ru>
2022-09-02 11:17:04 +0300

    security added, ensure current user

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,5 +1,6 @@
 class QuestionsController < ApplicationController
-  before_action :set_question, only: %i[update show destroy edit hide]
+  before_action :ensure_current_user, only: %i[update destroy edit hide]
+  before_action :set_question_for_current_user, only: %i[update destroy edit hide]
 
   def create
     @question = Question.create(question_params)
@@ -28,6 +29,7 @@ class QuestionsController < ApplicationController
   end
 
   def show
+    @question = Question.find(params[:id])
   end
 
   def index
@@ -45,12 +47,16 @@ class QuestionsController < ApplicationController
 
   private
 
+  def ensure_current_user
+    redirect_with_alert unless current_user.present?
+  end
+
   def question_params
     params.require(:question).permit(:body, :user_id)
   end
 
-  def set_question
-    @question = Question.find(params[:id])
+  def set_question_for_current_user
+    @question = current_user.questions.find(params[:id])
   end
 end
 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: %i[show edit update destroy]
+  before_action :authorize_user, only: %i[edit update destroy]
+
   def create
     @user = User.new(user_params)
 
@@ -16,12 +19,10 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = User.find(params[:id])
+
   end
 
   def update
-    @user = User.find(params[:id])
-
     if @user.update(user_params)
       redirect_to root_path, notice: 'Данные пользователя обновлены'
     else
@@ -31,7 +32,6 @@ class UsersController < ApplicationController
   end
 
   def destroy
-    @user = User.find(params[:id])
     @user.destroy
 
     session.delete(:user_id)
@@ -39,12 +39,19 @@ class UsersController < ApplicationController
   end
 
   def show
-    @user = User.find(params[:id])
     @questions = @user.questions
     @question = Question.new(user: @user)
   end
 
   private
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def authorize_user
+    redirect_with_alert unless current_user == @user
+  end
+
   def user_params
     params.require(:user).permit(
       :name, :nickname, :email, :password, :password_confirmation
